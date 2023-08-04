31f043866ff3015eb87fdd1f976da89d9504de7f
santhoshn20004 <127004239+santhoshn20004@users.noreply.github.com>
2023-08-04 15:37:46 +0530

    Delete mailingvariables.php

--- mailing/mailingvariables.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $mail_host = "officialsanthosh.n2004@gmail.com";
-    $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
-    $mail_sender_name = "Website Form";
-?>
